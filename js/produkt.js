00f4906a7c54173017559a8f1e28ea62625882a0
Alan <ale@kea.dk>
2024-09-03 14:09:21 +0200

    betinget_visning

---
+++ js/produkt.js
@@ -0,0 +1,20 @@
+// med urlParams kan vi fiske id'et ud af URL'en og vise det rigtige produkt
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+const url = `https://kea-alt-del.dk/t7/api/products/${id}`;
+
+function getProduct() {
+  fetch(url)
+    .then((res) => res.json())
+    .then(visProdukt);
+}
+
+function visProdukt(produkt) {
+  document.querySelector(".purchaseBox h3").textContent = produkt.productdisplayname;
+  document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
+  document.querySelector("img").alt = produkt.productdisplayname;
+  // etc. med de øvrige data
+}
+
+getProduct();
---
+++ js/produktliste.js
@@ -0,0 +1,32 @@
+window.addEventListener("DOMContentLoaded", hentData);
+
+const url = `https://kea-alt-del.dk/t7/api/products?limit=15`;
+const skabelon = document.querySelector("template").content;
+const container = document.querySelector("main");
+
+function hentData() {
+  fetch(url)
+    .then((res) => res.json())
+    .then((produkter) => visProdukter(produkter));
+}
+
+function visProdukter(produkter) {
+  produkter.forEach((produkt) => {
+    const kopi = skabelon.cloneNode(true);
+    kopi.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp`;
+    kopi.querySelector("img").alt = produkt.productdisplayname;
+    kopi.querySelector("h3").textContent = produkt.productdisplayname;
+    kopi.querySelector(".price span").textContent = produkt.price;
+    kopi.querySelector("a").href = `product.html?id=${produkt.id}`;
+    if (produkt.soldout) {
+      kopi.querySelector("article").classList.add("soldOut");
+    }
+    if (produkt.discount) {
+      kopi.querySelector("article").classList.add("onSale");
+      kopi.querySelector(".discounted p span").textContent = Math.round(produkt.price - (produkt.price * produkt.discount) / 100);
+      kopi.querySelector(".discounted p+p span").textContent = produkt.discount;
+    }
+
+    container.appendChild(kopi);
+  });
+}
