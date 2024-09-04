476bca1194dc04dbe920c554c9d35a6bf5fce300
maat0001kea <maat0001@stud.kea.dk>
2024-09-04 19:19:40 +0200

    productV1

--- js/produktliste.js
+++ js/produktliste.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", hentData);
 
-const url = `https://kea-alt-del.dk/t7/api/products?limit=15`;
+const url = `https://kea-alt-del.dk/t7/api/products?limit=16`;
 const skabelon = document.querySelector("template").content;
 const container = document.querySelector("main");
 
@@ -13,7 +13,9 @@ function hentData() {
 function visProdukter(produkter) {
   produkter.forEach((produkt) => {
     const kopi = skabelon.cloneNode(true);
-    kopi.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp`;
+    kopi.querySelector(
+      "img"
+    ).src = `https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp`;
     kopi.querySelector("img").alt = produkt.productdisplayname;
     kopi.querySelector("h3").textContent = produkt.productdisplayname;
     kopi.querySelector(".price span").textContent = produkt.price;
@@ -23,7 +25,9 @@ function visProdukter(produkter) {
     }
     if (produkt.discount) {
       kopi.querySelector("article").classList.add("onSale");
-      kopi.querySelector(".discounted p span").textContent = Math.round(produkt.price - (produkt.price * produkt.discount) / 100);
+      kopi.querySelector(".discounted p span").textContent = Math.round(
+        produkt.price - (produkt.price * produkt.discount) / 100
+      );
       kopi.querySelector(".discounted p+p span").textContent = produkt.discount;
     }
 
